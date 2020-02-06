6238eb1d4ca3c94d1fbf01b68fff2c00c0a9da4c
Kenny Mui <klmui@wisc.edu>
2020-02-06 11:50:17 -0600

    Add MainActivity

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -2,9 +2,38 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        double dollars = 0;
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String dollarsString = myTextField.getText().toString();
+
+        // Convert dollarsString to double
+        try {
+            dollars = Double.parseDouble(dollarsString);
+            // Valid double
+            goToActivity2(dollars);
+        } catch(NumberFormatException e) {
+            // Invalid double
+            myTextField.setText("");
+            Toast.makeText(MainActivity.this, "Invalid number of dollars", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void goToActivity2(double dollars) {
+        // Use intent to indicate where to send data
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("dollars", dollars);
+        // Switch activity
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
